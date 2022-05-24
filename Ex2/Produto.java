0a45abcb16d584fc7f085c248cc8a9337a60d650
BernardoCesar <bernardocesar2002@gmail.com>
2022-05-24 17:18:55 -0300

    To panquequinha
    Panquequinha das ideias

--- Ex2/Produto.java
+++ Ex2/Produto.java
@@ -1,8 +1,53 @@
 package Ex2;
 
+import javax.swing.JOptionPane;
+
 public class Produto {
-    String nome;
-    String dt_validade;
-    int qt_estoque;
+    private String nome;
+    private String dt_validade;
+    private int qt_estoque;
+
+    public Produto(){
+
+    }
     
+    public Produto(String nome, String dt_validade, int qt_estoque){
+        this.nome = nome;
+        this.dt_validade = dt_validade;
+        this.qt_estoque = qt_estoque;
+    }
+    
+    public void setDt_validade(String dt_validade) {
+        this.dt_validade = dt_validade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setQt_estoque(int qt_estoque) {
+        this.qt_estoque = qt_estoque;
+    }
+
+    public String getDt_validade() {
+        return dt_validade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getQt_estoque() {
+        return qt_estoque;
+    }
+    
+    public void diminuirEstoque(int valor){
+        if(valor>qt_estoque){
+            JOptionPane.showMessageDialog(null, "Estoque insuficiente("+qt_estoque+")");
+        }
+        qt_estoque=qt_estoque-valor;
+        }
+
+
+    @Override
+    public String toString() {
+        return "{Nome: " + getNome() +" |Data de Validade(AAAA/MM/DD): "+ getDt_validade() +" | Estoque: "+getQt_estoque() +"}";
+    }
+
 }
--- Ex2/Util.java
+++ Ex2/Util.java
@@ -1,23 +1,38 @@
 package Ex2;
+
+import javax.swing.JOptionPane;
+
 public class Util {
 
-    public static void reservar() {
+        static Lista<Produto> listaP= new Lista<Produto>();
 
+    public static void cadastrarP() {
+        String dt_validade, nome;
+        int qt_estoque;
+
+        nome=JOptionPane.showInputDialog("Insira o nome do produto:");
+        dt_validade=JOptionPane.showInputDialog("Insira a data de validade (AAAA/MM/DD):");
+        qt_estoque=Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade do produto:"));
+        Produto produto=new Produto(nome, dt_validade, qt_estoque);
+        if(listaP.pesquisar(produto)!=null){
+            JOptionPane.showMessageDialog(null, "O produto já foi cadastrado");
+        } else {
+            listaP.inserir(produto);
+            JOptionPane.showMessageDialog(null, "O produto foi cadastrado! :)");
+        }
     }
 
-    public static void pesquisar() {
+    public static void listarP() {
+        No<Produto> aux= listaP.inicio;
+        for(int i=0; i<listaP.total;i++){
+            
+        }
         
     }
 
-    public static void imprimirReserva() {
+    public static void venderP() {
         
     }
 
-    public static void imprimirEspera() {
-        
-    }
-    
-    public static void cancelar() {
-        
-    }
+   
 }
