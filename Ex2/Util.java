b6bcf066e342e4a3f58c739892a1811ac222cbe3
Fernando Zafani <zafani07@gmail.com>
2022-05-24 14:13:33 -0300

    uppp

--- Ex2/Util.java
+++ Ex2/Util.java
@@ -1,4 +1,23 @@
 package Ex2;
 public class Util {
+
+    public static void reservar() {
+
+    }
+
+    public static void pesquisar() {
+        
+    }
+
+    public static void imprimirReserva() {
+        
+    }
+
+    public static void imprimirEspera() {
+        
+    }
     
+    public static void cancelar() {
+        
+    }
 }
