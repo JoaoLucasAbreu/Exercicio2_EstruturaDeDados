068de77ef2c904205c823e3b666b71390de93c4a
BernardoCesar <bernardocesar2002@gmail.com>
2022-05-27 15:03:56 -0300

    V1.0
    To bilu teteia

--- Ex2/Util.java
+++ Ex2/Util.java
@@ -14,6 +14,9 @@ public class Util {
         nome=JOptionPane.showInputDialog("Insira o nome do produto:");
         dt_validade=JOptionPane.showInputDialog("Insira a data de validade (AAAA/MM/DD):");
         qt_estoque=Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade do produto:"));
+        if(qt_estoque<=0){
+            JOptionPane.showMessageDialog(null, "O produto não pode ser cadastrado sem estoque");
+        }else{
         Produto produto=new Produto(nome, dt_validade, qt_estoque);
         if(listaP.pesquisar(produto)!=null){
             JOptionPane.showMessageDialog(null, "Este produto já foi cadastrado");
@@ -22,15 +25,34 @@ public class Util {
             JOptionPane.showMessageDialog(null, "O produto foi cadastrado! :)");
         }
     }
+    }
 
     public static void listarP() {
+
+        Lista<Produto> copia= new Lista<Produto>();
         No<Produto> aux=listaP.inicio;
-        for(int i=0; i<listaP.total;i++){
-            JOptionPane.showMessageDialog(null, aux.dado.toString());
+        while(aux!=null){
+            copia.inserir(aux.dado);
             aux=aux.dir;
         }
+
+        for(int i=0; i<listaP.total;i++){
+            aux=copia.inicio; 
+            No<Produto> aux2=aux.dir;      
+            while(aux2!=null){
+                if(aux.dado.getDt_validade().compareTo(aux.dir.dado.getDt_validade())>=0){
+                    aux=aux2;
+                }      
+                aux2=aux2.dir;
+            }
+        
+            JOptionPane.showMessageDialog(null, aux.dado.toString());
+            copia.remover(aux.dado);
+        }
+
         
     }
+
 
     public static void venderP() {
         int valor;
